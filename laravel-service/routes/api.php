0017b19171825f2747a02678a7fa73b38d29e30d
masoud.vafaei <masoud.vafaei@videoslots.com>
2025-04-21 13:45:22 +0200

    fix laravel version

--- laravel-service/routes/api.php
+++ laravel-service/routes/api.php
@@ -2,10 +2,35 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProtectedController;
 
-Route::middleware('keycloak')->group(function () {
-    Route::get('/protected', [ProtectedController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/public', function () {
+    return response()->json([
+        'message' => 'This is a public endpoint',
+        'service' => 'Laravel Service'
+    ]);
+});
+
+Route::middleware('auth:api')->get('/protected', function (Request $request) {
+    return response()->json([
+        'message' => 'This is a protected endpoint',
+        'service' => 'Laravel Service',
+        'user' => $request->user()
+    ]);
 });
 
 Route::get('/health', function () {
